53ec4c15656d850f32db67f1a8e2d19f9a28fae2
ZelboK <danialjavady96@gmail.com>
2023-05-14 10:57:20 -0400

    update git ignore

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/12893/Desktop/CG resources/AppliedMathLearning/pbrt/exercises
-# Build directory: C:/Users/12893/Desktop/CG resources/AppliedMathLearning/pbrt/exercises/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
--- cmake-build-debug/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/12893/Desktop/CG resources/AppliedMathLearning/pbrt/exercises/test
-# Build directory: C:/Users/12893/Desktop/CG resources/AppliedMathLearning/pbrt/exercises/cmake-build-debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[Experiment]=] "Experiment")
-set_tests_properties([=[Experiment]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/12893/Desktop/CG resources/AppliedMathLearning/pbrt/exercises/test/CMakeLists.txt;11;add_test;C:/Users/12893/Desktop/CG resources/AppliedMathLearning/pbrt/exercises/test/CMakeLists.txt;0;")
